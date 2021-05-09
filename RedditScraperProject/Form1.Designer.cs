c7bed141e808b0d6020a853c9c17ae66ba68e112
whyster <aleksey.kalinovski@gmail.com>
2021-05-09 18:14:24 -0400

    offline update

---
+++ RedditScraperProject/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace RedditScraperProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.subredditBox = new System.Windows.Forms.TextBox();
+            this.saveLocationBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.countBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.countLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(6, 82);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.Start);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(29, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 19);
+            this.label1.TabIndex = 100;
+            this.label1.Text = "Subreddit: ";
+            // 
+            // subredditBox
+            // 
+            this.subredditBox.Location = new System.Drawing.Point(89, 36);
+            this.subredditBox.Name = "subredditBox";
+            this.subredditBox.Size = new System.Drawing.Size(127, 20);
+            this.subredditBox.TabIndex = 1;
+            // 
+            // saveLocationBox
+            // 
+            this.saveLocationBox.Location = new System.Drawing.Point(89, 59);
+            this.saveLocationBox.Name = "saveLocationBox";
+            this.saveLocationBox.Size = new System.Drawing.Size(127, 20);
+            this.saveLocationBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(46, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 19);
+            this.label2.TabIndex = 100;
+            this.label2.Text = "Count: ";
+            // 
+            // countBox
+            // 
+            this.countBox.Location = new System.Drawing.Point(89, 12);
+            this.countBox.Name = "countBox";
+            this.countBox.Size = new System.Drawing.Size(127, 20);
+            this.countBox.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(6, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 19);
+            this.label3.TabIndex = 100;
+            this.label3.Text = "Save Location:";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(87, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 23);
+            this.label4.TabIndex = 100;
+            this.label4.Text = "Post: ";
+            // 
+            // countLabel
+            // 
+            this.countLabel.Location = new System.Drawing.Point(117, 83);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(99, 23);
+            this.countLabel.TabIndex = 100;
+            this.countLabel.Text = "0/0";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.startButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 116);
+            this.Controls.Add(this.countLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.countBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.saveLocationBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.subredditBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Scraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox countBox;
+        private System.Windows.Forms.Label countLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox saveLocationBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox subredditBox;
+
+        #endregion
+    }
+}
---
+++ RedditScraperProject/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace RedditScraperProject
+{
+    public partial class Form1 : Form
+    {
+        static ReaderWriterLock locker = new ReaderWriterLock();
+        public string baseUrl = "http://old.reddit.com/r/";
+        public HttpClient webClient = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+            webClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            startButton.Enabled = false;
+            string subreddit = subredditBox.Text;
+            int count = Int32.Parse(countBox.Text);
+            int currentPosition = 0; 
+            string path = saveLocationBox.Text;
+
+            
+
+            HtmlDocument mainDoc = new HtmlDocument();
+            mainDoc.Load(webClient.GetStreamAsync($"{baseUrl}{subreddit}").Result);
+            var subDocs = mainDoc.DocumentNode.SelectNodes($"//p[@class = 'title']/a[contains(@href, '/r/{subreddit}')]");
+            
+            File.WriteAllText(path, "{\n");
+            Stack<Thread> threads = new Stack<Thread>();
+            while (currentPosition <= count) 
+            {
+                countLabel.Text = $"{currentPosition}/{count}";
+                int amount = (count - currentPosition) <= subDocs.Count ? (count - currentPosition) : (subDocs.Count);
+                Thread t = new Thread((() => subThreadProcess(amount, subDocs, path)));
+                threads.Push(t);
+                t.Start();
+
+                currentPosition += amount;
+                
+                countLabel.Text = $"{currentPosition}/{count}";
+                
+                if (currentPosition < count)
+                {
+                    try
+                    {
+                        mainDoc.Load(webClient.GetStreamAsync(mainDoc.DocumentNode.SelectNodes("//span[@class='next-button']/a[contains(@href, 'old.reddit.com')]")[0].GetAttributeValue("href", "tmp")).Result);
+                    }
+                    catch (Exception exception)
+                    {
+                    
+                        //File.WriteAllText("fuck.txt", mainDoc.Text);
+                        throw;
+                    }
+                }
+                else
+                {
+                    while (threads.Count != 0)
+                    {
+                        Thread t1 = threads.Pop();
+                        System.Diagnostics.Debug.WriteLine($"Joining main thread onto thread {t1.ManagedThreadId.ToString()}");
+                        t1.Join();
+                    }
+                    break;
+                }
+                
+                subDocs = mainDoc.DocumentNode.SelectNodes($"//p[@class = 'title']/a[contains(@href, '/r/{subreddit}')]");
+            } 
+             
+            //loop 
+            //until 25 
+            //then reload
+            try
+            {
+                locker.AcquireWriterLock(Int32.MaxValue);
+                File.AppendAllText(path, "\n}");
+                cleanFile(path);
+            }
+            finally
+            {
+                locker.ReleaseLock();
+            }
+            
+            
+            startButton.Enabled = true;
+        }
+
+        private void subThreadProcess(int acount, HtmlNodeCollection subDoc, string path)
+        {
+            String[] maxText = new string[acount];
+            System.Diagnostics.Debug.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId.ToString()} has started");
+            //Thread.CurrentThread.
+            string title, body;
+            for (int i = 0; i < acount; i++)
+            {
+                body = "";
+
+                HtmlDocument postDoc = new HtmlDocument();
+                postDoc.Load(webClient
+                    .GetStreamAsync($"http://old.reddit.com{subDoc[i].GetAttributeValue("href", "null")}").Result);
+                title = postDoc.DocumentNode.SelectNodes("//p[@class = 'title']/a")[0].InnerText.Trim();
+                try
+                {
+                    foreach (var node in postDoc.DocumentNode.SelectNodes(
+                        "//div[@class='entry unvoted']/div/form/div/div[@class='md']")[0].ChildNodes)
+                    {
+                        body += node.InnerText.Trim();
+                    }
+                }
+                catch (Exception e)
+                {
+                    body = "No body text";
+                }
+
+                maxText[i] = $"\"{title}\":\"{body}\",\n";
+            }
+            try
+            {
+                locker.AcquireWriterLock(Int32.MaxValue);
+                File.AppendAllLines(path, maxText);
+            }
+            finally
+            {
+                locker.ReleaseLock();
+            }
+            System.Diagnostics.Debug.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId.ToString()} has finished running");
+        }
+
+        private void cleanFile(string path)
+        {
+            /*
+             * &#39; = '
+             * â€™ = '
+             * â€œ
+             * â€
+             * \.([\w]) = \. $1
+             */
+            string text = File.ReadAllText(path);
+            Regex stuff = new Regex("\\.(.)");
+            text = stuff.Replace(text, ". $1");
+            
+            text = text.Replace("â€œ", "");
+            text = text.Replace("â€", "");
+
+            text = text.Replace("â€˜", "'");
+            text = text.Replace("&#39;", "'");
+            text = text.Replace("â€™", "'");
+
+            text = text.Replace("&quot;", "\\\"");
+            text = text.Replace("\n", "");
+            text = text.Replace("\",\"", "\",\n\"");
+            text = text.Replace(",}", "}");
+            File.WriteAllText(path, text);
+        }
+    }
+}
